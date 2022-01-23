c898b7e26c082bf6120b9a6de1cdfc76ebe4af4f
braveheart <395352228@qq.com>
2022-01-23 09:10:55 +0800

    xx

--- app/src/main/java/com/havefun/androidstudy/MainActivity.java
+++ app/src/main/java/com/havefun/androidstudy/MainActivity.java
@@ -1,61 +1,51 @@
 package com.havefun.androidstudy;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.SuppressLint;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.text.Editable;
-import android.text.InputFilter;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
-import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ForegroundColorSpan;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
 
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.havefun.androidstudy.databinding.ActivityMainBinding;
-import com.havefun.common.Constants;
-import com.havefun.shortcode.Cinema;
-import com.havefun.shortcode.RealMovie;
-import com.havefun.shortcode.ShortCodeActivity;
 import com.havefun.shortcode.activity.JetpackActivity;
-import com.havefun.view.WidgetActivity;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 
 @Route(path = "/test/activity")
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
     private TextView textView;
+    public final int MSG_TEST = 5;
+    Handler handler = new Handler(Looper.getMainLooper()) {
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what){
+                case MSG_TEST:
+                    // do something
+                    break;
+                default:
+                    // do something
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +57,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.tvLaunchThirdParty.setOnClickListener(this);
 
         startActivity(new Intent(this, JetpackActivity.class));
+
+        Message msg = Message.obtain();
+        msg.what = MSG_TEST;
+        handler.sendMessage(msg);
     }
 
 
